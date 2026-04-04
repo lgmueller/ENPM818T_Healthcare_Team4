1c67546c1c97ae3b4eb0ba807e9088402c28c0cb
Lillian Mueller <lmuelle1@umd.edu>
2026-04-04 13:08:04 -0400

    adding tables to schema.sql

---
+++ postgresql/schema.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE healthcare_db;
+
+CREATE TABLE patient ();
+
+CREATE TABLE phone_numbers ();
+
+CREATE TABLE emails ();
+
+CREATE TABLE emergency_contacts ();
+
+CREATE TABLE patient_condition ();
+
+CREATE TABLE condition ();
+
+CREATE TABLE patient_allergy ();
+
+CREATE TABLE provider ();
+
+CREATE TABLE medical_license ();
+
+CREATE TABLE board_certs ();
+
+CREATE TABLE specialty ();
+
+CREATE TABLE medical_degrees ();
+
+CREATE TABLE DEA_no ();
+
+CREATE TABLE physician_availability ();
+
+CREATE TABLE admission ();
+
+CREATE TABLE appointment ();
+
+CREATE TABLE facility ();
+
+CREATE TABLE hospital_room ();
+
+CREATE TABLE prescription ();
+
+CREATE TABLE refill_history ();
+
+CREATE TABLE medication ();
+
+CREATE TABLE lab_order ();
+
+CREATE TABLE lab_test ();
+
+CREATE TABLE insurance ();
+
+CREATE TABLE insurance_claim ();
+
+CREATE TABLE payment ();
+
+CREATE TABLE insurance_diagnosis_codes ();
+
+CREATE TABLE insurance_procedures_codes ();
